b98917036fa53d91fbd2872f9c8f12134ec765cc
yitbarek <yitbarek@gmail.com>
2022-10-21 12:41:30 +0200

    all changes

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,3 +1,3 @@
-GATEWAY_URL = 'http://gateway' + ":5002"
-MATH_URL = 'http://math' + ':5001'
-STRING_URL = 'http://string' + ':5003'
+GATEWAY_URL = 'http://gateway:' + '5000' # add <PORT1>
+MATH_URL = 'http://math_py:' + '5000' # add <PORT2>
+STRING_URL = 'http://string_rust:' + '5000' # add <PORT3>
