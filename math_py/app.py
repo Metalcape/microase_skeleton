3e802916b38af4bcd33f5420addb387f42ffb9a2
Dario Capecchi <dario.capecchi.m@gmail.com>
2022-10-21 12:51:59 +0200

    update

--- math_py/app.py
+++ math_py/app.py
@@ -14,23 +14,43 @@ def add():
 
 @app.route('/sub')
 def sub():
-    return ""
     #TODO: complete this function
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s= a - b), 200) # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) # HTTP 400 BAD REQUEST
 
 @app.route('/mul')
 def mul():
-    return ""
     #TODO: complete this function
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s= a * b), 200) # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) # HTTP 400 BAD REQUEST
 
 @app.route('/div')
 def div():
-    return ""
-    #TODO: complete this function. 
+    #TODO: complete this function
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b and b!=0:
+        return make_response(jsonify(s= a / b), 200) # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) # HTTP 400 BAD REQUEST
 
 @app.route('/mod')
 def mod():
-    return ""
-    #TODO: complete this function. 
+    #TODO: complete this function
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b and b!=0:
+        return make_response(jsonify(s= a % b), 200) # HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) # HTTP 400 BAD REQUEST
 
 
 def create_app():
